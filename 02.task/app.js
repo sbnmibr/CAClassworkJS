f77f08775ba720625f9b7513b85bac79ff11fa92
|Sbnmibr <sbnmibrahimova500@gmail.com>
2023-10-31 13:54:20 +0400

    fix

---
+++ 02.task/app.js
@@ -0,0 +1,51 @@
+ let weather = 16;
+ if (weather>=22){
+    console.log(`${weather} nazik geyin` );
+   
+ } else if (weather>=15){
+   console.log(`${weather} pencek geyin `);
+ } else{
+   console.log(`${weather} qalin geyin` );
+
+ }
+
+
+ let sait = "b"
+ if ( sait="a"){
+    console.log( `${sait} herf saitdir`);
+ } else  {
+   console.log(`${weather} herf samitdir`);
+ }
+
+let a =25
+let b=52
+let c=88 
+if ( a>b && a>c){
+   console.log(`max number ${a}`);
+} else if(b>c && b>a) {
+   console.log(`max number ${b}`);
+}else {
+   console.log(`max number ${c}`);
+}
+
+let exam =100 ;
+if ( a<100){
+  
+} else if( grade>90){
+   console.log(`exam grade "A"`);
+} else if(grade>80){
+   console.log(`exam grade "B"`);
+} else if (grade>70){
+   console.log(`exam grade "C"`);
+} else if (grade>60){
+   console.log(`exam grade "d"`);
+} else if (grade>50){
+   console.log(`exam grade "e"`);
+}
+
+
+
+
+
+
+
