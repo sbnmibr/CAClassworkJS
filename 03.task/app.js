9a4d8051cd814b7b0f81476472bb9aafefadc8a6
|Sbnmibr <sbnmibrahimova500@gmail.com>
2023-11-01 13:36:03 +0400

    fix

---
+++ 03.task/app.js
@@ -0,0 +1,33 @@
+// // for (let i = 1; i <= 100; i++) {    TAS1?
+//   if (i % 2 === 0) console.log(i);
+// }
+
+// // for (let i = 10; i <100; i++) {       TASK1
+//   if (i % 10 === 7) console.log(i);
+// }
+// for (let i = 10; i <88; i++) {
+//   if (i % 11 === 0) console.log(i);
+// }
+
+// let str = "Sebish";
+// let strlength = str.length;
+// for (i = 0; i < strlength; i++) {
+//   console.log(str[i]);
+// }
+// let a = 10;
+// for (i = 0; i <= 10; i++) {
+//   if (a % i === 0) console.log(i);
+// }
+// let i=50;
+// while(i){
+//   if(i%5==0  && i%5==0) console.log("fizzbuzz");
+//   else if (i%3===0)console.log("buzz");
+//   }
+
+
+  let a=3;
+  let fact=1;
+  for (let i=1;i<a;i++){
+    fact*=i 
+    console.log(factorial ${fact});
+  }
